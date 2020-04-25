f66e8b816ba18c8af839234186a941f8afeb034b
SungJin <osj4872@gmail.com>
2020-04-25 13:17:59 +0900

    파일 정리

--- 2020-04-19-bak/nginx/api-doc/README.md
+++
@@ -1,4 +0,0 @@
-# apiDoc_init
-* https://github.com/apidoc/apidoc
-* apidoc을 이용하여 api document 생성
-* API 문서가 변할 시 jenkins를 통해 dist 디렉토리로 배포한다.
--- 2020-04-19-bak/upload.sh
+++
@@ -1,8 +0,0 @@
-# with key
-if [ $1 ]
-  then 
-    scp ./$1 fonnie@fonnie.xyz:infra_init/$1
-    exit
-fi
-
-scp -r ./* fonnie@fonnie.xyz:infra_init
